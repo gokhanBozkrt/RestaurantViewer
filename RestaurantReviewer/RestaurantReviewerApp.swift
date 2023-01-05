600ab8abe6646d93dc3486feb8aeddc189433f31
Gokhan Bozkurt <gkhnbzkrt1000@gmail.com>
2023-01-05 14:14:23 +0300

    Initial Commit

---
+++ RestaurantReviewer/RestaurantReviewerApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestaurantReviewerApp.swift
+//  RestaurantReviewer
+//
+//  Created by Gökhan Bozkurt on 5.01.2023.
+//
+
+import SwiftUI
+
+@main
+struct RestaurantReviewerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
